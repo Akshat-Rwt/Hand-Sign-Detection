394dd245a591ca68f109cb3169c133f188933052
Akshat Rawat <111777625+Akshat-Rwt@users.noreply.github.com>
2025-03-10 11:15:14 +0530

    Crop the Image
    1. You get the image but for Data Collection you can crop the image form the videoCapture.
    2. Use BoundingBox to crop the image.
    3. Give the starting height/width and the end height/width.

--- dataCollection.py
+++ dataCollection.py
@@ -8,6 +8,14 @@ detector = HandDetector(maxHands=1) # This is the function in cvzome for detecti
 while True:
     success  ,img = cap.read()
     hands , img = detector.findHands(img) # Find the Hands
+
+    # Crop the image
+    if hands:
+        hand = hands[0]
+        x,y,w,h = hand['bbox'] # Bounding Box
+        imgCrop = img[y:y+h , x:x+w] # Starting height/width and the ending height/width
+        cv2.imshow("ImageCrop", imgCrop)
+
     cv2.imshow("Image", img)
     cv2.waitKey(1)
 
